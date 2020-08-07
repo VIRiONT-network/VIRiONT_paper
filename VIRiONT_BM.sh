2be7a42f45a3349a6c19cf72bedc31df45aecb9b
Ext-Hadrien REGUE01 <regueex@chu-lyon.fr@di3225su.chu-lyon.fr>
2020-08-07 15:02:40 +0200

    maj launcher

--- VIRiONT_BM.sh
+++ VIRiONT_BM.sh
@@ -4,17 +4,13 @@
 ##########################    CONFIGURATION    #################################
 ################################################################################
 #fastq location
-#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS/" #Define path where the "barcode*" are stored
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/DATA/"
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS/" #Define path where the "barcode*" are stored
 #output location
-#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/RESULT_HBV_PARIS/" #Define path where storing analysis results 
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/RESULTS/"
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/RESULT_HBV_PARIS/" #Define path where storing analysis results 
 #custom reference file to use
-#ref_loc="ref/HBV_REF.fasta" # Path to fastafile containing genotype sequences for blast
-ref_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/POLIO.fasta"
+ref_loc="ref/HBV_REF.fasta" # Path to fastafile containing genotype sequences for blast
 #metadata location for the custom reference
-#ref_table="analysis/table_analysis.csv" # Table location containing reference list for the blastn analysis. /!\ Column must correspond to the fasta headers in ref_loc.
-ref_table="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/table_analysis.csv" 
+ref_table="analysis/table_analysis.csv" # Table location containing reference list for the blastn analysis. /!\ Column must correspond to the fasta headers in ref_loc.
 #min length for read filtering
 min_length=1500
 #max length for read filtering
@@ -36,12 +32,12 @@ mem_cost=32000 #Define number of threads to use for the analysis
 ################################################################################
 #########################    LAUNCH SNAKEMAKE    ###############################
 ################################################################################
-snakemake -s VIRiONT.py \
+snakemake -s VIRiONT_BM.py \
     --use-conda \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
     --config PathToData=$data_loc \
-             PathToResult=${result_loc}"OLD_VERSION/" \
+             PathToResult=${result_loc}"BEST_MATCHING_ANALYSIS/" \
              PathToReference=$ref_loc \
              AnalysisTable=$ref_table \
              Lmin=$min_length \
@@ -49,32 +45,6 @@ snakemake -s VIRiONT.py \
              headcrop=$head \
              tailcrop=$tail \
              variantfrequency=$Vfreq
-
-
-snakemake -s VIRiONT_MI1.py \
-    --use-conda \
-    --core $thread_number \
-    --resources mem_mb=$mem_cost \
-    --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
-             PathToReference=$ref_loc \
-             AnalysisTable=$ref_table \
-             Lmin=$min_length \
-             Lmax=$max_length \
-             headcrop=$head \
-             tailcrop=$tail \
-             multiinf=$MI_cutoff \
-             variantfrequency=$Vfreq
-
-snakemake -s VIRiONT_MI2.py \
-    --use-conda \
-    --core $thread_number \
-    --resources mem_mb=$mem_cost \
-    --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
-             PathToReference=$ref_loc \
-             variantfrequency=$Vfreq 
-
 
 chmod 777 -R $data_loc
 ################################################################################
--- VIRiONT_MI.sh
+++ VIRiONT_MI.sh
@@ -4,17 +4,17 @@
 ##########################    CONFIGURATION    #################################
 ################################################################################
 #fastq location
-#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS/" #Define path where the "barcode*" are stored
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/DATA/"
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS/" #Define path where the "barcode*" are stored
+#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/DATA/"
 #output location
-#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/RESULT_HBV_PARIS/" #Define path where storing analysis results 
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/RESULTS/"
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/RESULT_HBV_PARIS/" #Define path where storing analysis results 
+#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/RESULTS/"
 #custom reference file to use
-#ref_loc="ref/HBV_REF.fasta" # Path to fastafile containing genotype sequences for blast
-ref_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/POLIO.fasta"
+ref_loc="ref/HBV_REF.fasta" # Path to fastafile containing genotype sequences for blast
+#ref_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/POLIO.fasta"
 #metadata location for the custom reference
-#ref_table="analysis/table_analysis.csv" # Table location containing reference list for the blastn analysis. /!\ Column must correspond to the fasta headers in ref_loc.
-ref_table="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/table_analysis.csv" 
+ref_table="analysis/table_analysis.csv" # Table location containing reference list for the blastn analysis. /!\ Column must correspond to the fasta headers in ref_loc.
+#ref_table="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/table_analysis.csv" 
 #min length for read filtering
 min_length=1500
 #max length for read filtering
@@ -36,60 +36,29 @@ mem_cost=32000 #Define number of threads to use for the analysis
 ################################################################################
 #########################    LAUNCH SNAKEMAKE    ###############################
 ################################################################################
-snakemake -s VIRiONT.py \
-    --use-conda \
-    --core $thread_number \
-    --resources mem_mb=$mem_cost \
-    --config PathToData=$data_loc \
-             PathToResult=${result_loc}"OLD_VERSION/" \
-             PathToReference=$ref_loc \
-             AnalysisTable=$ref_table \
-             Lmin=$min_length \
-             Lmax=$max_length \
-             headcrop=$head \
-             tailcrop=$tail \
-             variantfrequency=$Vfreq
-
-
 snakemake -s VIRiONT_MI1.py \
     --use-conda \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
     --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
+             PathToResult=${result_loc}"MI_DETECTION/" \
              PathToReference=$ref_loc \
              AnalysisTable=$ref_table \
              Lmin=$min_length \
              Lmax=$max_length \
              headcrop=$head \
              tailcrop=$tail \
-             multiinf=$MI_cutoff \
-             variantfrequency=$Vfreq
+             multiinf=$MI_cutoff
 
 snakemake -s VIRiONT_MI2.py \
     --use-conda \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
     --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
+             PathToResult=${result_loc}"MI_DETECTION/" \
              PathToReference=$ref_loc \
              variantfrequency=$Vfreq 
 
-
 chmod 777 -R $data_loc
-################################################################################
 
-#snakemake -s VIRiONT.py --rulegraph \
-#    --use-conda \
-#    --core $thread_number \
-#    --resources mem_mb=$mem_cost \
-#    --config PathToData=$data_loc \
-#             PathToResult=$result_loc \
-#             PathToReference=$ref_loc \
-#             AnalysisTable=$ref_table \
-#             Lmin=$min_length \
-#             Lmax=$max_length \
-#             headcrop=$head \
-#             tailcrop=$tail \
-#             variantfrequency=$Vfreq | dot -Tpng > documents/workflow.png
 
