bef7449873f3445ed8a9fef13ac131d0eb27d50b
Ext-Hadrien REGUE01 <regueex@chu-lyon.fr@di3226su.chu-lyon.fr>
2020-08-19 16:00:06 +0200

    fix for testing last data

--- VIRiONT_MI.sh
+++ VIRiONT_MI.sh
@@ -4,21 +4,17 @@
 ##########################    CONFIGURATION    #################################
 ################################################################################
 #fastq location
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS/" #Define path where the "barcode*" are stored
-#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/DATA/"
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/CCharre/MinION_HBV/4_run_MIX_AOUT/VHB" #Define path where the "barcode*" are stored
 #output location
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/RESULT_HBV_PARIS/" #Define path where storing analysis results 
-#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/RESULTS/"
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/CCharre/MinION_HBV/4_run_MIX_AOUT/VHB/DRIdri_analyseMI" #Define path where storing analysis results 
 #custom reference file to use
 ref_loc="ref/HBV_REF.fasta" # Path to fastafile containing genotype sequences for blast
-#ref_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/POLIO.fasta"
 #metadata location for the custom reference
 ref_table="analysis/table_analysis.csv" # Table location containing reference list for the blastn analysis. /!\ Column must correspond to the fasta headers in ref_loc.
-#ref_table="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/table_analysis.csv" 
 #min length for read filtering
-min_length=1500
+min_length=500
 #max length for read filtering
-max_length=3500
+max_length=1500
 #Remove N nucleotide for primer 5'
 head=0
 #Remove N nucleotide for primer 3'
@@ -28,7 +24,7 @@ MI_cutoff=10
 #minor variant frequency
 Vfreq=0.5 
 #core number
-thread_number=10 #Define number of threads to use for the analysis
+thread_number=8 #Define number of threads to use for the analysis
 #memory cost in mb
 mem_cost=32000 #Define number of threads to use for the analysis
 ################################################################################
@@ -41,7 +37,7 @@ snakemake -s VIRiONT_MI1.py \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
     --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
+             PathToResult=$result_loc \
              PathToReference=$ref_loc \
              AnalysisTable=$ref_table \
              Lmin=$min_length \
@@ -55,10 +51,8 @@ snakemake -s VIRiONT_MI2.py \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
     --config PathToData=$data_loc \
-             PathToResult=${result_loc}"MI_VERSION/" \
+             PathToResult=$result_loc \
              PathToReference=$ref_loc \
              variantfrequency=$Vfreq 
-
-chmod 777 -R $data_loc
 
 
