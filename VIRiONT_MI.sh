e7a256d34c56282becf6f3748d912a70e8a3cfa1
charreca <60978441+charreca@users.noreply.github.com>
2020-09-16 12:09:15 +0200

    Update VIRiONT_MI.sh

--- VIRiONT_MI.sh
+++ VIRiONT_MI.sh
@@ -8,20 +8,11 @@
 #######################    GENERAL PARAMETERS    ###############################
 ################################################################################
 #fastq location / Define path where the "barcode*" rep are stored
-#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS" 
-#M1
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/DATA_Melange_1_F2" 
-#M2
-#data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS" 
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_PARIS" 
 #output location / Define path where storing analysis results 
-#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/TEST_MI/" 
-#F1
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/EV_MELANGE_1_F2_TRIM_1000_3500" 
-#F2
-#result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/TEST_MI/" 
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/TEST_MI/" 
 #custom reference file to use /  Path to fastafile containing reference sequences for blast
-#ref_loc="ref/HBV_REF.fasta" 
-ref_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/POLIO/POLIO.fasta"
+ref_loc="ref/HBV_REF.fasta" 
 #core number / Define number of threads to use for the analysis
 thread_number=8
 #memory cost in mb / Define number of threads to use for the analysis
@@ -36,9 +27,9 @@ min_length=1000
 #max length for read filtering
 max_length=3500
 #Remove N nucleotide for primer 5'
-head=22
+head=0
 #Remove N nucleotide for primer 3'
-tail=23
+tail=0
 ################################################################################
 
 ################################################################################
@@ -54,7 +45,7 @@ mincov=20
 ###################    MULTI-INFECTION PARAMETER    ############################
 ################################################################################
 #Multi infection threshold cutoff in percent / cutoff=count(Blastref_reads)/count(majoritaryBlastref_reads)*100
-MI_cutoff=30
+MI_cutoff=20
 ################################################################################
 
 
@@ -74,8 +65,7 @@ snakemake -s VIRiONT_MI1.py \
              Lmax=$max_length \
              headcrop=$head \
              tailcrop=$tail \
-             multiinf=$MI_cutoff \
-             threadnumblast=$thread_number
+             multiinf=$MI_cutoff
 
 snakemake -s VIRiONT_MI2.py \
     --use-conda \
