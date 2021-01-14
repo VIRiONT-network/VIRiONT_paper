87acd439c72b700c3a71181584aee879254be7f2
HadrienRegue <hadrien.regue@univ-lyon1.fr>
2021-01-14 09:50:54 +0100

    save

--- launch_VIRiONT.sh
+++ launch_VIRiONT.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#k5start -U -f /home/chu-lyon.fr/regueex/login.kt -- nohup ./launch_VIRiONT.sh &
 ################################################################################
 ##########################    CONFIGURATION    #################################
 ################################################################################
@@ -8,13 +8,13 @@
 #######################    GENERAL PARAMETERS    ###############################
 ################################################################################
 #fastq location / Define path where the "barcode*" rep are stored
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HBV_TEST/" 
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HDV_TEST/" 
 #output location / Define path where storing analysis results 
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/CORRECTED_FLAIR_DATA/" 
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/HDV_SGT_NEWREFWITHPRIMER_PILEUPQ20/" 
 #custom reference file to use /  Path to fastafile containing reference sequences for blast
-ref_loc="ref/HBV_REF.fasta" 
+ref_loc="ref/HDV_subtype_V2.fasta" 
 #core number / Define number of threads to use for the analysis
-thread_number=4
+thread_number=8
 #memory cost in mb / Define number of threads to use for the analysis
 mem_cost=62000
 ################################################################################
@@ -23,15 +23,15 @@ mem_cost=62000
 #################    TRIMMING/FILTERING PARAMETERS    ##########################
 ################################################################################
 #min length for read filtering
-min_length=1500
+min_length=500
 #max length for read filtering
-max_length=3500
+max_length=2000
 #average read quality for filtering
 quality=0
 #Remove N 5' nucleotides from each filtered read 
-head=0
+head=23
 #Remove N 3' nucleotides from each filtered read 
-tail=0
+tail=23
 ################################################################################
 
 ################################################################################
@@ -56,7 +56,7 @@ mincov=20
 ###################    MULTI-INFECTION PARAMETER    ############################
 ################################################################################
 #Multi infection threshold cutoff in percent / cutoff=count(Blastref_reads)/count(majoritaryBlastref_reads)*100
-MI_cutoff=20
+MI_cutoff=30
 ################################################################################
 
 
@@ -78,7 +78,7 @@ snakemake -s smk_VIRiONT_1.py -p  \
              tailcrop=$tail \
              multiinf=$MI_cutoff
 
-snakemake -s smk_VIRiONT_1.py -p \
+snakemake -s smk_VIRiONT_2.py -p \
     --use-conda \
     --core $thread_number \
     --resources mem_mb=$mem_cost \
@@ -96,5 +96,3 @@ snakemake -s smk_VIRiONT_1.py -p \
              multiinf=$MI_cutoff
 
 chmod -R 777 $result_loc
-
-
