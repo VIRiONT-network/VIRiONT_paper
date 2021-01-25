de02e6c9afbeb688b583263025d951850ad95bb6
Hadrien Regue <hadrien.regue@univ-lyon1.fr>
2021-01-25 19:15:56 +0100

    maj launcher

--- launch_VIRiONT.sh
+++ launch_VIRiONT.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#k5start -U -f /home/chu-lyon.fr/regueex/login.kt -- nohup ./launch_VIRiONT.sh &
+#k5start -U -f /home/regueex@chu-lyon.fr/login.kt -- nohup ./launch_VIRiONT.sh &
 ################################################################################
 ##########################    CONFIGURATION    #################################
 ################################################################################
@@ -8,37 +8,37 @@
 #######################    GENERAL PARAMETERS    ###############################
 ################################################################################
 #fastq location / Define path where the "barcode*" rep are stored
-data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/DATA_HDV_TEST/" 
+data_loc="/srv/nfs/ngs-stockage/NGS_Virologie/CCharre/MinION_HBV/VIRiONT_analyses_ARTICLE_VHB/" 
 #output location / Define path where storing analysis results 
-result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/HDV_SGT_NEWREFWITHPRIMER_PILEUPQ20/" 
+result_loc="/srv/nfs/ngs-stockage/NGS_Virologie/HadrienR/CARO_PIPELINE/HBV_SGT_1000-3500_PILEUPQ20/" 
 #custom reference file to use /  Path to fastafile containing reference sequences for blast
-ref_loc="ref/HDV_subtype_V2.fasta" 
+ref_loc="ref/HBV_REF.fasta" 
 #core number / Define number of threads to use for the analysis
 thread_number=8
 #memory cost in mb / Define number of threads to use for the analysis
-mem_cost=62000
+mem_cost=32000
 ################################################################################
 
 ################################################################################
 #################    TRIMMING/FILTERING PARAMETERS    ##########################
 ################################################################################
 #min length for read filtering
-min_length=500
+min_length=1000
 #max length for read filtering
-max_length=2000
+max_length=3500
 #average read quality for filtering
 quality=0
 #Remove N 5' nucleotides from each filtered read 
-head=23
+head=21
 #Remove N 3' nucleotides from each filtered read 
-tail=23
+tail=21
 ################################################################################
 
 ################################################################################
 #################    VARIANT CALLING PARAMETERS    ##########################
 ################################################################################
 #maximum depth for samtools mpileup
-maxdepth=1000000
+maxdepth=0
 #base quality cutoff for samtools mpileup
 basequal=20
 ################################################################################
@@ -56,7 +56,7 @@ mincov=20
 ###################    MULTI-INFECTION PARAMETER    ############################
 ################################################################################
 #Multi infection threshold cutoff in percent / cutoff=count(Blastref_reads)/count(majoritaryBlastref_reads)*100
-MI_cutoff=30
+MI_cutoff=20
 ################################################################################
 
 
