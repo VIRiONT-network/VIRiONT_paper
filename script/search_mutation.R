bbf72b62f58468957c6ab6f2d24e44cb1bf97010
HadrienRegue <hadrien.regue@univ-lyon1.fr>
2021-04-09 14:07:15 +0200

    modif for matching subtype and genotype

--- script/search_mutation.R
+++ script/search_mutation.R
@@ -8,16 +8,26 @@ argv <- commandArgs(TRUE)
 #Parametres
 vcf_input<-argv[1]
 tablemut_input<-argv[2]
+filter_threshold<-as.numeric(as.character(argv[3]))
+window_pos<-as.numeric(as.character(argv[4]))
 
-file_output_PC<-argv[3]
-file_output_BCP<-argv[4]
-file_output_DS<-argv[5]
-file_output_RT<-argv[6]
-file_output_DPS1<-argv[7]
-file_output_DPS2<-argv[8]
-file_output_DHBx<-argv[9]
-file_output_C<-argv[10]
+file_output_PC<-argv[5]
+file_output_BCP<-argv[6]
+file_output_DS<-argv[7]
+file_output_RT<-argv[8]
+file_output_DPS1<-argv[9]
+file_output_DPS2<-argv[10]
+file_output_DHBx<-argv[11]
+file_output_C<-argv[12]
 
+file_output_PC_F<-argv[13]
+file_output_BCP_F<-argv[14]
+file_output_DS_F<-argv[15]
+file_output_RT_F<-argv[16]
+file_output_DPS1_F<-argv[17]
+file_output_DPS2_F<-argv[18]
+file_output_DHBx_F<-argv[19]
+file_output_C_F<-argv[20]
 
 #Parametres
 #vcf_input<-"MUTATIONS/BC08_GTA_PRECONS.txt"
@@ -51,7 +61,8 @@ searchMUT_CODON<-function(vcf,mut_table,mutation){
                           values_to = "REF_POS")
   mut_table$NUM_CODON<-mut_table$Position_EcoR1
   mut_table<-mut_table[,c("REF_POS","POS_TYPE","NUM_CODON")]
-  
+  #move window if no primer in reference
+  mut_table$REF_POS<-mut_table$REF_POS+window_pos
   table_vcf_mut<-merge(vcf,mut_table,by="REF_POS")
   
   CODON_REF<-aggregate(table_vcf_mut$REF,list(table_vcf_mut$base_status,table_vcf_mut$NUM_CODON),paste,collapse="")
@@ -76,6 +87,8 @@ searchMUT_CODON<-function(vcf,mut_table,mutation){
 searchMUT_NT<-function(vcf,mut_table,mutation){
   mut_table<-subset(table_mut,Region==mutation)
   mut_table<-mut_table[,c("Position_EcoR1","NUC1_P3")]
+  #move window if no primer in reference
+  mut_table$NUC1_P3<-mut_table$NUC1_P3+window_pos
   table_vcf_mut<-merge(vcf,mut_table,by.x="REF_POS",by.y="NUC1_P3")
   table_vcf_mut$Mutation_name<-paste0(table_vcf_mut$REF,as.character(table_vcf_mut$Position_EcoR1),table_vcf_mut$base)
   table_vcf_mut<-table_vcf_mut[,c("REFERENCE","REF_POS","Position_EcoR1","REF","total_count",
@@ -104,11 +117,24 @@ total_count<-aggregate(table_vcf$count,by=list(table_vcf$REF_POS),sum)
 colnames(total_count)<-c("REF_POS","total_count")
 
 table_vcf<-merge(table_vcf,total_count,by="REF_POS")
-
 table_vcf$freq<-round(table_vcf$count/table_vcf$total_count*100,digits=2)
 
+#get table to use
+reference<-unique(as.character(table_vcf$REFERENCE))
+reference<-str_replace(reference,"P3_GT","")
+reference<-str_replace(reference,"GT","")
+if(grepl("A",reference)) table<-"mutation_GTA.csv"
+if(grepl("B",reference)) table<-"mutation_GTB.csv"
+if(grepl("C",reference)) table<-"mutation_GTC.csv"
+if(grepl("D",reference)) table<-"mutation_GTD.csv"
+if(grepl("E",reference)) table<-"mutation_GTE.csv"
+if(grepl("F",reference)) table<-"mutation_GTF.csv"
+if(grepl("G",reference)) table<-"mutation_GTG.csv"
+if(grepl("H",reference)) table<-"mutation_GTH.csv"
+if(grepl("I",reference)) table<-"mutation_GTI.csv"
+
 #read mutation table
-table_mut<-read.csv2(tablemut_input)
+table_mut<-read.csv2(paste0(tablemut_input,table))
 table_mut$Region<-as.character(table_mut$Region)
 
 
@@ -124,6 +150,19 @@ table_vcf_DPS1<-searchMUT_CODON(table_vcf,table_mut,"Domaine PreS1")
 table_vcf_DPS2<-searchMUT_CODON(table_vcf,table_mut,"Domaine PreS2")
 table_vcf_DHBx<-searchMUT_CODON(table_vcf,table_mut,"Domaine HBx")
 
+#filtered tables
+############# Nucleotid mut research #############
+table_vcf_PC_filter<-subset(table_vcf_PC,freq>=filter_threshold)
+table_vcf_BCP_filter<-subset(table_vcf_BCP,freq>=filter_threshold)
+
+############# Protein mut research #############
+table_vcf_RT_filter<-subset(table_vcf_RT,freq>=filter_threshold)
+table_vcf_C_filter<-subset(table_vcf_C,freq>=filter_threshold)
+table_vcf_DS_filter<-subset(table_vcf_DS,freq>=filter_threshold)
+table_vcf_DPS1_filter<-subset(table_vcf_DPS1,freq>=filter_threshold)
+table_vcf_DPS2_filter<-subset(table_vcf_DPS2,freq>=filter_threshold)
+table_vcf_DHBx_filter<-subset(table_vcf_DHBx,freq>=filter_threshold)
+
 ############# Write tables #############
 write.csv2(table_vcf_BCP,file_output_BCP,row.names = F,quote = F)
 write.csv2(table_vcf_PC,file_output_PC,row.names = F,quote = F)
@@ -133,3 +172,13 @@ write.csv2(table_vcf_DPS1,file_output_DPS1,row.names = F,quote = F)
 write.csv2(table_vcf_DPS2,file_output_DPS2,row.names = F,quote = F)
 write.csv2(table_vcf_DHBx,file_output_DHBx,row.names = F,quote = F)
 write.csv2(table_vcf_C,file_output_C,row.names = F,quote = F)
+
+############# Write tables #############
+write.csv2(table_vcf_BCP_filter,file_output_BCP_F,row.names = F,quote = F)
+write.csv2(table_vcf_PC_filter,file_output_PC_F,row.names = F,quote = F)
+write.csv2(table_vcf_RT_filter,file_output_RT_F,row.names = F,quote = F)
+write.csv2(table_vcf_DS_filter,file_output_DS_F,row.names = F,quote = F)
+write.csv2(table_vcf_DPS1_filter,file_output_DPS1_F,row.names = F,quote = F)
+write.csv2(table_vcf_DPS2_filter,file_output_DPS2_F,row.names = F,quote = F)
+write.csv2(table_vcf_DHBx_filter,file_output_DHBx_F,row.names = F,quote = F)
+write.csv2(table_vcf_C_filter,file_output_C_F,row.names = F,quote = F)
